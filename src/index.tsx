7aec517c2c029fb04ed28c93f5f187e934027a0a
VINChe1990 <vinche1990@gmail.com>
2025-07-23 15:42:59 +0800

    Fix app link

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {createRoot} from 'react-dom/client';
-import App from './components/app/app.tsx';
+import App from './components/App/App.tsx';
 import {DefaultData} from './mocks/mocks.ts';
 
 const root = createRoot(
