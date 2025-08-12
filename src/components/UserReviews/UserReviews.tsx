a639018861cb031e07e0a70c5fde37ca9bea85fc
VINChe1990 <vinche1990@gmail.com>
2025-08-12 15:57:47 +0800

    Fix PR issues

--- src/components/UserReviews/UserReviews.tsx
+++ src/components/UserReviews/UserReviews.tsx
@@ -146,7 +146,7 @@ const UserReviews = ({ offerId }: UserReviewsProps) => {
           <textarea className="reviews__textarea form__textarea" id="review" name="review" placeholder="Tell how was your stay, what you like and what can be improved" onChange={handleCommentChange} value={newReview.comment}></textarea>
           <div className="reviews__button-wrapper">
             <p className="reviews__help">
-              To submit review please make sure to set <span className="reviews__star">rating</span> and describe your stay with at least <b className="reviews__text-amount">50 characters</b>.
+              To submit review please make sure to set <span className="reviews__star">rating</span> and describe your stay with at least <span className="reviews__text-amount">50 characters</span>.
             </p>
             <button className="reviews__submit form__submit button" type="submit" disabled={!isValidReview} onClick={handleClickSubmit}>Submit</button>
           </div>
