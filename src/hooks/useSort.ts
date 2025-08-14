c7a4e95b37d23043e3bf5ada5d60b54f306be9eb
VINChe1990 <vinche1990@gmail.com>
2025-08-14 17:37:02 +0800

    Fix defaut sort

--- src/hooks/useSort.ts
+++ src/hooks/useSort.ts
@@ -4,18 +4,18 @@ import { selectSortType } from '../store/selectors';
 
 function useSort(places: IPlace[]): IPlace[] {
   const sortType = useAppSelector(selectSortType);
-  return [...places].sort((a, b) => {
-    switch (sortType) {
-      case 'Price: low to high':
-        return a.price - b.price;
-      case 'Price: high to low':
-        return b.price - a.price;
-      case 'Top rated first':
-        return b.rating - a.rating;
-      default:
-        return 0;
-    }
-  });
+  switch (sortType) {
+    case 'Price: low to high':
+      return [...places].sort((a, b) => a.price - b.price);
+    case 'Price: high to low':
+      return [...places].sort((a, b) => b.price - a.price);
+    case 'Top rated first':
+      return [...places].sort((a, b) => b.rating - a.rating);
+    case 'Popular':
+    default:
+      // Возвращаем копию исходного массива без сортировки
+      return [...places];
+  }
 }
 
 export default useSort;
